860685d2fe1405a1b794858a069d1a77c46e243a
ravinaNG <ravina18@navgurukul.org>
2019-11-15 10:28:57 +0530

    selection short

--- challengingQuestions/selectionShort.js
+++ challengingQuestions/selectionShort.js
@@ -1,4 +1,5 @@
 const arr = [6, 1, 7, 8, 9, 3, 5, 4, 2];
+
 function findMinNum(index, arr){
     var index_of_min_index;
     var min = arr[index];
@@ -12,14 +13,19 @@ function findMinNum(index, arr){
 }
 
 var index = 0;
+var exchanged_num;
 for(index; index<arr.length; index++){
-    console.log("shorting.....")
     var arr_index = index;
     var min_index = findMinNum(arr_index, arr);
     var min_num = arr[min_index];
     exchanged_num = arr[index];
-    arr[index] = min_num;
-    arr[min_index] = exchanged_num;
+    if(min_num == undefined){
+        continue;
+    }
+    else{
+        arr[index] = min_num;
+        arr[min_index] = exchanged_num;
+    }
 }
 
 console.log(arr);
