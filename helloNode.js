2013477e8dbe756ae10ca726a745323b9f135877
ravinaNG <ravina18@navgurukul.org>
2019-11-26 21:53:55 +0530

    using localHost

--- helloNode.js
+++ helloNode.js
@@ -1,6 +1,6 @@
 // Load HTTP module
 const http = require("http");
-const hostName = '127.0.0.1';
+// const hostName = 'Ravina';
 const port = 3000;
 
 // Create HTTP server and listen on port 3000 for requests
@@ -9,9 +9,10 @@ const server = http.createServer((req, res) => {
     // res.statusCode = 200;
     // res.setHeader('Content-Type', 'text/plain');
     res.end('Hello world\n');
-});
+}).listen(8080);
+console.log('server is running on the port 8080 !!!');
 
 // listen for request on port 3000, and as a callback have the port listened on logged
-server.listen(port, hostName, () => {
-    console.log(`Server running at http://${hostName}:${port}/`);
-});
+// server.listen(port, hostName, () => {
+//     console.log(`Server running at http://${hostName}:${port}/`);
+// });
