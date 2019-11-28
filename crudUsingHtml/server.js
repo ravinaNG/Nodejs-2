7bf63a1e11e17ddca624656193ec79db4063d2da
ravinaNG <ravina18@navgurukul.org>
2019-11-28 18:50:05 +0530

    crud with HTML

--- crudUsingHtml/server.js
+++ crudUsingHtml/server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
 app.get('/index.html', function(req, res) {
     res.sendFile( __dirname + '/' + 'index.html');
 })
